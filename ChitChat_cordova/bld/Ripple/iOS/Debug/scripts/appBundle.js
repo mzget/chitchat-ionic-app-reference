fe16cd0bcc1fa9832bf7881d0f1eb791f1808009
Nattapon Rattajak <nattapon.r@live.com>
2015-09-15 10:33:36 +0700

    test call getCompanyInfo api.

--- ChitChat_cordova/bld/Ripple/iOS/Debug/scripts/appBundle.js
+++ ChitChat_cordova/bld/Ripple/iOS/Debug/scripts/appBundle.js
@@ -29,12 +29,12 @@ var BlankCordovaApp1;
     };
 })(BlankCordovaApp1 || (BlankCordovaApp1 = {}));
 /// <reference path="./typings/tsd.d.ts" />
-//requirejs.config({
-//    paths: {
-//        jquery: '../js/jquery.min',
-//        cryptojs: '../js/crypto-js/crypto-js'
-//    }
-//});
+requirejs.config({
+    paths: {
+        jquery: '../js/jquery.min',
+        cryptojs: '../js/crypto-js/crypto-js'
+    }
+});
 // Directly call the RequireJS require() function and from here
 // TypeScript's external module support takes over
 //require(["../../scripts/server/serverImplemented"]); 
