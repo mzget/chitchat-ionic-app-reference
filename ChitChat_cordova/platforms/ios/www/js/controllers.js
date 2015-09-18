a20434f0257399820c8b8f7e49d9e7cd65ab0f0b
prosupagon <pro.k.supagon@gmail.com>
2015-09-18 14:08:47 +0700

    pao

--- ChitChat_cordova/platforms/ios/www/js/controllers.js
+++ ChitChat_cordova/platforms/ios/www/js/controllers.js
@@ -31,9 +31,9 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+	$scope.settings = {
+		logOut: true,
+	};
 });
 
 
