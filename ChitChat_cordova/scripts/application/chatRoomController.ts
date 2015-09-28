9551f6bd6a1bfe85b986b8207696b50ec57c4969
Nattapon Rattajak <nattapon.r@live.com>
2015-09-28 14:11:45 +0700

    update : room controller interface.

--- ChitChat_cordova/scripts/application/chatRoomController.ts
+++ ChitChat_cordova/scripts/application/chatRoomController.ts
@@ -1,8 +1,17 @@
-﻿
-class ChatRoomController {
+﻿interface IChatRoomController {
+    onChat(data);
+    onLeaveRoom(data);
+    onRoomJoin(data);
+    onMessageRead(dataEvent);
+    onGetMessagesReaders(dataEvent);
+}
+
+class ChatRoomController implements IChatRoomController {
     public chatMessages = [];
 
     onChat(chatMessageImp) {
+        console.log("Implement chat msg hear..", chatMessageImp);
+
         var secure = new SecureService();
         if (chatMessageImp.type === ContentType[ContentType.Text]) {
             secure.decryptWithSecureRandom(chatMessageImp.body, (err, res) => {
@@ -19,5 +28,21 @@ class ChatRoomController {
         else {
             this.chatMessages.push(chatMessageImp);
         }
+    }
+
+    onLeaveRoom(data) {
+
+    }
+
+    onRoomJoin(data) {
+
+    }
+
+    onMessageRead(dataEvent) {
+
+    }
+
+    onGetMessagesReaders(dataEvent) {
+
     }
 }
