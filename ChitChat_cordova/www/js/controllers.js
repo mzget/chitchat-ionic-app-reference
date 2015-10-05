10f6cdfdaa55a173f92069a85f774dacae3f1cb5
ATOMX ATOM <atomx_@msn.com>
2015-10-05 17:48:49 +0700

    Open Gallery

--- ChitChat_cordova/www/js/controllers.js
+++ ChitChat_cordova/www/js/controllers.js
@@ -228,6 +228,44 @@ angular.module('starter.controllers', [])
 	$scope.settings = {
 		logOut: true,
 	};
+})
+
+.controller('AccountCreate',function($scope) {
+    $scope.images = "http://placehold.it/50x50";
+})
+
+.controller('ImageController', function($scope, $ionicPlatform, $ionicActionSheet, ImageService, FileService) {
+ 
+  $ionicPlatform.ready(function() {
+    $scope.images = FileService.images();
+    $scope.$apply();
+  });
+ 
+  $scope.urlForImage = function(imageName) {
+    var trueOrigin = cordova.file.dataDirectory + imageName;
+    return trueOrigin;
+  }
+ 
+  $scope.addMedia = function() {
+    $scope.hideSheet = $ionicActionSheet.show({
+      buttons: [
+        { text: 'Take photo' },
+        { text: 'Photo from library' }
+      ],
+      titleText: 'Add images',
+      cancelText: 'Cancel',
+      buttonClicked: function(index) {
+        $scope.addImage(index);
+      }
+    });
+  }
+ 
+  $scope.addImage = function(type) {
+    $scope.hideSheet();
+    ImageService.handleMediaDialog(type).then(function() {
+      $scope.$apply();
+    });
+  }
 }); // <-- LAST CONTROLLER
 
 
