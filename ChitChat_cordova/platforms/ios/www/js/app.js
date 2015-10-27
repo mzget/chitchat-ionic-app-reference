9b015169ef7348794df05e49def280f8c47cca92
Nattapon Rattajak <nattapon.r@live.com>
2015-10-27 12:13:22 +0700

    update platform folder.

--- ChitChat_cordova/platforms/ios/www/js/app.js
+++ ChitChat_cordova/platforms/ios/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'spartan.controllers', 'starter.directives', 'spartan.chat', 'spartan.media', 'starter.services', 'ngCordova'])
+angular.module('starter', ['ionic', 'spartan.controllers', 'starter.directives', 'spartan.chat', 'spartan.media', 'spartan.group', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
@@ -81,7 +81,17 @@ angular.module('starter', ['ionic', 'spartan.controllers', 'starter.directives',
 		views: {
 			'tab-group': {
 				templateUrl: 'templates/tab-group-members.html',
-				controller: 'GroupMembersCtrl'
+				controller: 'viewGroupMembersCtrl'
+			}
+		}
+	})
+
+	.state('tab.group-members-invite', {
+		url: '/group/members/:chatId/invite',
+		views: {
+			'tab-group': {
+				templateUrl: 'templates/tab-account-invite.html',
+				controller: 'editMemberGroup'
 			}
 		}
 	})
