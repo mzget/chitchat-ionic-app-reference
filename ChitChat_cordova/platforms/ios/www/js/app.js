8a3a0d56d18cfb239bcff1ab5b52233ac030aeea
Nattapon Rattajak <nattapon.r@live.com>
2015-10-29 16:59:14 +0700

    update platform/ios folder.

--- ChitChat_cordova/platforms/ios/www/js/app.js
+++ ChitChat_cordova/platforms/ios/www/js/app.js
@@ -5,7 +5,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'spartan.controllers', 'spartan.chatslog', 'starter.directives', 'spartan.chat', 'spartan.media', 'spartan.group', 'starter.services', 'ngCordova'])
+angular.module('starter',
+    ['ionic', 'spartan.controllers', 'spartan.home', 'spartan.chatslog', 'starter.directives', 'spartan.chat', 'spartan.media', 'spartan.group',
+        'spartan.services', 'spartan.notify','ngCordova'])
 
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
