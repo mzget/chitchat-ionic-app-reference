4f3a624d64e67b50ab91f10c93a52840998f2bbb
prosupagon <pro.k.supagon@gmail.com>
2015-11-02 23:17:28 +0700

    chatlog add chatDetail

--- ChitChat_cordova/www/js/controllers/chatslogController.js
+++ ChitChat_cordova/www/js/controllers/chatslogController.js
@@ -5,9 +5,14 @@
         .module('spartan.chatslog', [])
         .controller('chatslogController', chatslogController);
 
-    chatslogController.$inject = ['$location', '$scope'];
-
-    function chatslogController($location, $scope) {
+    chatslogController.$inject = ['$location', '$scope', '$timeout', 'roomSelected'];
+			
+	var roomAccess = [];
+	var roomAccessLength = 0;
+	var myRoomAccess = [];
+	var myRoomAccessCount = 0;
+	
+    function chatslogController($location, $scope, $timeout, roomSelected) {
         /* jshint validthis:true */
         var vm = this;
         vm.title = 'chatslogController';
@@ -15,10 +20,111 @@
         activate();
 
         function activate() { }
+				
+        $scope.$on('$ionicView.enter', function(){ 
+            navShow();
+		});
 
         var dataManager = main.getDataManager();
-        $scope.roomAccess = dataManager.myProfile.roomAccess;
+				
+		$scope.myProfile = dataManager.myProfile;
+		$scope.orgMembers = dataManager.orgMembers;
+		$scope.roomAccess = [];
+		getRoomAccess();
+		var refresh = function () 
+		{		
+			$scope.roomAccess = myRoomAccess;
+			console.log('reload');
+		
+			console.log(roomAccess);
+			console.log(myRoomAccess);
+			
+			$timeout(refresh, 1000);
+		} 
+		$timeout(refresh, 1000);
+		
+		
+		$scope.gotoChat = function (accessId) 
+		{		
+			var accessLength = myRoomAccess.length;
+			for(var i=0; i<accessLength; i++)
+			{
+				if( myRoomAccess[i]['_id'] == accessId )
+				{
+					switch( myRoomAccess[i]['type'] )
+					{
+						case 0:
+							var group = main.getDataManager().orgGroups[accessId];
+							roomSelected.setRoom(group);
+							location.href = '#/tab/chats/chat/' + accessId;
+							break;
+						case 1:
+							var group = main.getDataManager().projectBaseGroups[accessId];
+							roomSelected.setRoom(group);
+							location.href = '#/tab/chats/chat/' + accessId;
+							break;
+						case 2:
+							var group = main.getDataManager().privateGroups[accessId];
+							roomSelected.setRoom(group);
+							location.href = '#/tab/chats/chat/' + accessId;
+							break;
+						case 3:
+							server.getPrivateChatRoomId(dataManager.myProfile._id, accessId, function result(err, res) {
+								console.log(JSON.stringify(res));
+								var room = JSON.parse(JSON.stringify(res.data));
 
-        console.log('you have access %s rooms', dataManager.myProfile.roomAccess.length);
+								$scope.chat = function () {
+									roomSelected.setRoom(room);
+									location.href = '#/tab/chats/chat/' + room._id;
+								};
+								
+								$scope.openViewContactProfile = function(id) {
+									location.href = '#/tab/chats/member/' + id;
+								}
+							});
+							break;
+					}
+				}
+			}	
+		};
     }
+	
+	function getRoomAccess()
+	{
+		console.log('getRoomAccess: ');
+		
+		roomAccess = dataManager.myProfile.roomAccess;
+		roomAccessLength = roomAccess.length;
+		
+		getRoomInfo();
+	}
+	
+	function getRoomInfo()
+	{
+		if( myRoomAccessCount < roomAccessLength )
+		{
+			console.log( 'wait: ' + myRoomAccessCount + '/' + roomAccessLength );	
+			server.getRoomInfo(roomAccess[myRoomAccessCount]['roomId'], function(err, res){				
+				if( res['code'] == 200 )
+				{
+					console.log( res['data']['_id'] );
+					var data = res;
+					myRoomAccess.push(data['data']);
+					/*
+					myRoomAccess[ res['data']['_id'] ] = {};
+					myRoomAccess[ res['data']['_id'] ]['roomId'] = 'I';
+					myRoomAccess[ res['data']['_id'] ]['accessTime'] = 'J';		
+					*/				
+				}
+				
+				if( myRoomAccessCount+1 == roomAccessLength )
+				{
+					console.log( 'last' );
+				}else{
+					myRoomAccessCount++;
+					getRoomInfo(myRoomAccessCount);
+				}	
+			});
+		}
+	}	
 })();
