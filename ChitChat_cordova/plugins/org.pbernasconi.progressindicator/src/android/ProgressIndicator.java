0e3ea725ee328b3e1956a57ce3a85ba152813745
ATOMX ATOM <atomx_@msn.com>
2015-11-02 11:47:01 +0700

    plugin

--- ChitChat_cordova/plugins/org.pbernasconi.progressindicator/src/android/ProgressIndicator.java
+++ ChitChat_cordova/plugins/org.pbernasconi.progressindicator/src/android/ProgressIndicator.java
@@ -1,4 +1,4 @@
-package org.apache.cordova.progressindicator;
+package org.apache.cordova.plugin;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -9,42 +9,19 @@ import android.app.ProgressDialog;
 public class ProgressIndicator extends CordovaPlugin {
 
     private ProgressDialog progressIndicator = null;
-	private static final String LOG_TAG = "ProgressIndicator";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
-            show();
-            callbackContext.success();
-            return true;
-        } else if (action.equals("showSimple")) {
-            show();
-            callbackContext.success();
-            return true;
-        } else if (action.equals("showSimpleWithLabel")) {
-            String title = args.getString(1);
-            show(title);
-            callbackContext.success();
-            return true;
-        } else if (action.equals("showSimpleWithLabelDetail")) {
-            String title = args.getString(1);
-            String text = args.getString(2);
-            show(title, text, true);
-            callbackContext.success();
-            return true;
-        } else if (action.equals("showText")) {
-            String title = args.getString(1);
-			String text = args.getString(2);
-            show(title, text, false);
+            String text = args.getString(0);
+            show(text);
             callbackContext.success();
             return true;
         } else if (action.equals("hide")) {
             hide();
             callbackContext.success();
             return true;
-        } else {
-            callbackContext.error("Not supported call. On Android we only support show, showSimple, showSimpleWithLabel and showSimpleWithLabelDetail");
-		}
+        }
 
         return false;
     }
@@ -54,33 +31,8 @@ public class ProgressIndicator extends CordovaPlugin {
      *
      * @param text - Message to display in the Progress Dialog
      */
-    public void show() {
-        progressIndicator = new ProgressDialog(cordova.getActivity());
-		progressIndicator.show();
-    }
-
-    /**
-     * This show the ProgressDialog
-     *
-     * @param text - Message to display in the Progress Dialog
-     */
     public void show(String text) {
-        progressIndicator = new ProgressDialog(cordova.getActivity());
-		progressIndicator.setTitle(text);
-		progressIndicator.show();
-    }
-
-    /**
-     * This show the ProgressDialog
-     *
-     * @param text - Message to display in the Progress Dialog
-     */
-    public void show(String title, String detail, Boolean withTitle) {
-        progressIndicator = new ProgressDialog(cordova.getActivity());
-		if(withTitle)
-			progressIndicator.setTitle(title);
-		progressIndicator.setMessage(detail);
-		progressIndicator.show();
+        progressIndicator = ProgressDialog.show(this.cordova.getActivity(), "", text);
     }
 
     /**
