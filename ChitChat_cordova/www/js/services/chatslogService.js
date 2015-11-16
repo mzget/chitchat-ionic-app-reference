d13c2cd8c5d0415ff11d70dd2cbbebebecb47042
Nattapon Rattajak <nattapon.r@live.com>
2015-11-16 12:24:34 +0700

    introduce chatslog service.

---
+++ ChitChat_cordova/www/js/services/chatslogService.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('spartan.services')
+        .factory('chatslogService', chatslogService);
+
+    chatslogService.$inject = ['$http'];
+
+    function chatslogService($http) {
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() { }
+    }
+})();
