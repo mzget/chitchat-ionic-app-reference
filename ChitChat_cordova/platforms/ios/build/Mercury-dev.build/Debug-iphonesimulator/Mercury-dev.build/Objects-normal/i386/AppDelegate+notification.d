cd4eb6c281bb3f3812d89808a6d591554a6bd2b3
Nattapon Rattajak <nattapon.r@live.com>
2016-01-21 23:13:20 +0700

    no message

--- ChitChat_cordova/platforms/ios/build/Mercury-dev.build/Debug-iphonesimulator/Mercury-dev.build/Objects-normal/i386/AppDelegate+notification.d
+++ ChitChat_cordova/platforms/ios/build/Mercury-dev.build/Debug-iphonesimulator/Mercury-dev.build/Objects-normal/i386/AppDelegate+notification.d
@@ -23,4 +23,46 @@ dependencies: \
   /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/build/emulator/include/Cordova/NSData+Base64.h \
   /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/build/emulator/include/Cordova/NSDictionary+Extensions.h \
   /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/build/emulator/include/Cordova/UIDevice+Extensions.h \
-  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/build/emulator/include/Cordova/CDVJSON.h
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/build/emulator/include/Cordova/CDVJSON.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/Parse.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/ParseClientConfiguration.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFConstants.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFACL.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFAnalytics.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Bolts.framework/Headers/BFTask.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Bolts.framework/Headers/BFCancellationToken.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Bolts.framework/Headers/BFCancellationTokenRegistration.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Bolts.framework/Headers/BFDefines.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFAnonymousUtils.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFUser.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFObject.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFSubclassing.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFAnonymousUtils+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFCloud.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFCloud+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFCloud+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFConfig.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFConfig+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFFile.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFFile+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFFile+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFGeoPoint.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFObject+Subclass.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFObject+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFObject+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFQuery.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFQuery+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFQuery+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFRelation.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFRole.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFSession.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFUser+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFUser+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFUserAuthenticationDelegate.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFInstallation.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFNetworkActivityIndicatorManager.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFPush.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFPush+Synchronous.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFPush+Deprecated.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFProduct.h \
+  /Users/nattapon.r/Documents/chitchat-mercury/ChitChat_cordova/platforms/ios/Parse.framework/Headers/PFPurchase.h
