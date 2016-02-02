10ea643863e06e6a4ca746df53dcdf8d8a20e3b0
Nattapon Rattajak <nattapon.r@live.com>
2016-02-02 19:31:13 +0700

    success build mercury-ios v.0.0.5

--- ChitChat_cordova/platforms/ios/www/js/controllers/authController.js
+++ ChitChat_cordova/platforms/ios/www/js/controllers/authController.js
@@ -106,7 +106,6 @@
         }
 
         function activateNetworkService() {
-            $rootScope.webServer = networkService.getWebServer();
             networkService.regisSocketListener();
         }
 
