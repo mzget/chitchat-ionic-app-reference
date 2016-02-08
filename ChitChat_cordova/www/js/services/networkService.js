9e57dd06c2046b39e0df45a250af82a980a6c4a4
Nattapon Rattajak <nattapon.r@live.com>
2016-02-08 17:29:36 +0700

    add CordovaNetwork and networkInfo plugins for checking connection status.

--- ChitChat_cordova/www/js/services/networkService.js
+++ ChitChat_cordova/www/js/services/networkService.js
@@ -5,14 +5,31 @@
         .module('spartan.services')
         .factory('networkService', networkService);
 
-    networkService.$inject = ['$http', '$state', "localNotifyService"];
+    networkService.$inject = ['$http', '$state', '$rootScope', '$cordovaNetwork', "localNotifyService"];
 
-    function networkService($http, $state, localNotifyService) {
+    function networkService($http, $state, $rootScope, $cordovaNetwork, localNotifyService) {
         var service = {
+            init: init,
             regisSocketListener: regisSocketListener
         };
 
         return service;
+
+        function init() {
+            // listen for Online event
+            $rootScope.$on('$cordovaNetwork:online', function (event, networkState) {
+                var onlineState = networkState;
+                console.warn('network state', networkState);
+            })
+
+            // listen for Offline event
+            $rootScope.$on('$cordovaNetwork:offline', function (event, networkState) {
+                var offlineState = networkState;
+
+                console.warn('network state', networkState);
+                localNotifyService.makeToast("Network is unstable.");
+            })
+        }
 
         function regisSocketListener() {
             console.log("regis socket event.");
@@ -20,7 +37,14 @@
             server.setSocketComponent(socket);
             socket.onDisconnect =  function onDisconnect(reason) {
                 localNotifyService.makeToast("disconnected.");
-                $state.go("tab.login");
+
+                //@-- Todo..
+                if ($cordovaNetwork.isOnline()) {
+                    $state.go("tab.login");
+                }
+                else {
+                    //@ Stay working offline.
+                }
             }
         }
     }
